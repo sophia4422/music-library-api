d7b3a04a494d3ba67487f61e893f402dc586eb49
Tanveer Bassi <tanveer13a551@gmail.com>
2022-06-26 13:12:15 +0100

    routers initialized

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const routes = require("./routes");
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`listening to port on http://localhost:${PORT}`);
