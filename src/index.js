1b3480fc29a6a2911ac576187e74c96517dad3a5
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-26 12:26:09 +0100

    port

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`listening to port on http://localhost:${PORT}`);
+});
